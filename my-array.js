44b2ad8cc41f7fd97ec14f3db46e9b8980889d2f
Jittrin <Jittrin.Puttawetmongkol@gmail.com>
2026-07-02 11:56:55 +0700

    Add member

---
+++ my-array.js
@@ -0,0 +1,2 @@
+let food = ["Burger", "Pizza", "Malatang", "Sushi", "Medovik", "Pad Thai"];
+console.log(food[0]);
---
+++ my-object.js
@@ -0,0 +1,39 @@
+//"Burger", "Pizza", "Malatang", "Sushi", "Medovik", "Pad Thai"
+
+let GenmateA = [
+    {
+        id : 1,
+        name : "Base" , 
+        hometown : "Bangkok",
+        age : "31",
+        favoriteFood : "Sushi"
+        },
+    {        
+        id : 2,
+        name : "Faii" , 
+        hometown : "Korat",
+        age : "27",
+        favoriteFood : "Kapao"
+    },
+    {        
+        id : 3,
+        name : "Delta" , 
+        hometown : "Bangkok",
+        age : "27",
+        favoriteFood : "Everything jinggerbell"
+    },
+    {        
+        id : 4,
+        name : "Por" , 
+        hometown : "Bangkok",
+        age : "24",
+        favoriteFood : "Mala"
+    },
+    {        
+        id : 5,
+        name : "Touch" , 
+        hometown : "Bangkok",
+        age : "24",
+        favoriteFood : "Madovik"
+    },
+];
