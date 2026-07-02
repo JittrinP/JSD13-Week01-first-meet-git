919caddad7d12b8ec4973bb6c15b822c2a5c7dc6
Jittrin <Jittrin.Puttawetmongkol@gmail.com>
2026-07-02 13:36:52 +0700

    Add profile picture

--- my-object.js
+++ my-object.js
@@ -6,34 +6,39 @@ let GenmateA = [
         name : "Base" , 
         hometown : "Bangkok",
         age : "31",
-        favoriteFood : "Sushi"
-        },
+        favoriteFood : "Sushi",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Keanu_Reeves_2019.jpg/500px-Keanu_Reeves_2019.jpg"
+    },
     {        
         id : 2,
         name : "Faii" , 
         hometown : "Korat",
         age : "27",
-        favoriteFood : "Kapao"
+        favoriteFood : "Kapao",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ed/Scarlett_Johansson_%2829479903050%29.jpg/500px-Scarlett_Johansson_%2829479903050%29.jpg"
     },
     {        
         id : 3,
         name : "Delta" , 
         hometown : "Bangkok",
         age : "27",
-        favoriteFood : "Everything jinggerbell"
+        favoriteFood : "Everything jinggerbell",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Leonardo_Dicaprio_Cannes_2019.jpg/500px-Leonardo_Dicaprio_Cannes_2019.jpg"
     },
     {        
         id : 4,
         name : "Por" , 
         hometown : "Bangkok",
         age : "24",
-        favoriteFood : "Mala"
+        favoriteFood : "Mala",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Tom_Holland_by_Gage_Skidmore.jpg/500px-Tom_Holland_by_Gage_Skidmore.jpg"
     },
     {        
         id : 5,
         name : "Touch" , 
         hometown : "Bangkok",
         age : "24",
-        favoriteFood : "Madovik"
+        favoriteFood : "Madovik",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/Zendaya_2019_by_Glenn_Francis.jpg/500px-Zendaya_2019_by_Glenn_Francis.jpg"
     },
 ];
